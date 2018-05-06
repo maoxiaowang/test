68a6a5669cd54204facff4d7014ffa6a8ee05e99
Oliver <1225191678@qq.com>
2018-05-06 22:02:58 +0800

    sth

--- common/forms_helper.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-from django.forms.utils import ErrorDict
-import json
-
-
-def form_errors_to_list(errors):
-    assert isinstance(errors, ErrorDict)
-    return [item[0]['message'] for item in list(json.loads(errors.as_json()).values())]
---
+++ neutron/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NeutronConfig(AppConfig):
+    name = 'neutron'
